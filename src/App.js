00f7e14803d73db3916f027573505047238de7dc
Jedai Saboteur <dev@paradoxinversion.com>
2018-09-23 10:37:35 -0700

    Add hmr support; Add start script

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { hot } from "react-hot-loader";
 import "./App.css";
 
 class App extends Component {
@@ -11,4 +12,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default hot(module)(App);
